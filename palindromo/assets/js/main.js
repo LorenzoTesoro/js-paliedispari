7bf78831740d130537a4d790af3d4c7652e7678c
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-14 12:03:33 +0200

    add comments

---
+++ palindromo/assets/js/main.js
@@ -0,0 +1,10 @@
+/*  Consegna: chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+/* Strumenti */
+
+// 1. prompt - chiedere all'utente di inserire una parola
+// 2. if/else - per capire se la parola è palindroma
+//    2.1 - come faccio a dirgli di leggere prima da dx a sx e poi                viceversa?      
+// 2.1.1 - 1: converto la parola inserita dall'utente in un array (uso string split() )
+// 2.1.2 - 2: uso array.reverse() per invertire l'ordine dei caratteri
+// 2.1.3 - 3: faccio il check. parola1 = parola1.reverse ?
