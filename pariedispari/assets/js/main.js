c11a8995e3bc9abcf8e04ee3cbd78c9b16abdeab
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-14 15:35:22 +0200

    pariedispari refactoring

--- pariedispari/assets/js/main.js
+++ pariedispari/assets/js/main.js
@@ -1,23 +1,20 @@
 /* Consegna: L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
-// 1. L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 - prompt
-const playerChoice = prompt('pari o dispari');
-const playerNumber = Number(prompt("Scegli un numero da 1 a 5"));
-const pcNumber = Math.floor((Math.random() * 5) + 1);
-// 3. Sommiamo i due numeri (n inserito dall'utente + n casuale)
-const sum = playerNumber + pcNumber;
-// 4. Stabiliamo se la somma dei due numeri è pari o dispari
-function oddOrEven(number) {
-    if(number % 2 === 0){
+const playerChoice = prompt('pari o dispari'); // L’utente sceglie pari o dispari
+const playerNumber = Number(prompt("Scegli un numero da 1 a 5")); // inserisco un numero da 1 a 5
+const pcNumber = Math.floor((Math.random() * 5) + 1); // Genero un numero random (sempre da 1 a 5)
+
+// Sommo i due numeri e stabilisco se la somma dei due numeri è pari o dispari
+function oddOrEven(num1, num2) {
+    const sum = num1 + num2;
+    console.log(sum);
+    if(sum % 2 === 0){
         return "pari";
     } 
     return "dispari";
 }
-// console.log(sum);
-// console.log(oddOrEven(sum)); 
-
-// dichiaro chi ha vinto
-if (playerChoice == oddOrEven(sum)){
+// Dichiaro chi ha vinto
+if (playerChoice == oddOrEven(playerNumber, pcNumber)){
     console.log('Player1 wins!');
 } else {
     console.log ('Pc wins!');
