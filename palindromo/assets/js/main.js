966a9f1cdac3ffbf735bd7e1b629c52692eadb62
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-14 16:54:19 +0200

    add palindrome markup

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -1,26 +1,27 @@
 /*  Consegna: chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
 // chiedo all'utente di inserire una parola
-const inputElement = document.querySelector("input");
-const userWord = toString(inputElement);
 
 function palindromeCheck(word) {
-    const wordArray = word.split(''); // converto la parola inserita dall'utente in un array
-    const wordReverted = wordArray.reverse()// inverto l'ordine dei caratteri dell'array creata
+    const wordArray = word.split(''); /// converto la parola inserita dall'utente in un array
+    const wordReverted = wordArray.reverse();// inverto l'ordine dei caratteri dell'array creata
     const checkWord = wordReverted.join(""); // riconverto l'array in una stringa
 
     // check finale
     if(word == checkWord){
-        return "palindrome!";
-    } 
-    return "not palindrome!";
+        const palindrome = "Palindrome!"
+        document.querySelector('h2').innerHTML = `${palindrome}`
+    } else{
+        const notPalindrome = "Not palindrome!"
+        document.querySelector('h2').innerHTML = `${notPalindrome}`
+    }
+
 }
 
 const btnElement = document.querySelector("button");
+const inputElement = document.querySelector("input");
 
-btnElement.addEventListener('click', function (){
-    console.log(palindromeCheck(userWord));
-})
-
-
-// console.log(palindromeCheck(userWord));
+btnElement.addEventListener('click', function(){
+    const userWord = inputElement.value
+    palindromeCheck(userWord);
+});
