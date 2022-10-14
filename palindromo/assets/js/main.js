4f0a09b59c684123349a9e637f7dc96ea063e62c
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-14 14:11:54 +0200

    add function and styles

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -1,28 +1,18 @@
 /*  Consegna: chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
-/* Strumenti */
+// chiedo all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
 
-// 1. prompt - chiedo all'utente di inserire una parola
-// 2. converto la parola inserita dall'utente in un array (string.split())
-const word = prompt('Inserisci una parola');
-const wordArray = word.split('');
+function palindromeCheck(word) {
+    const wordArray = word.split(''); // converto la parola inserita dall'utente in un array (string.split())
+    const wordReverted = wordArray.reverse()// inverto l'ordine dei caratteri dell'array creata
+    const checkWord = wordReverted.join(""); // riconverto l'array in una stringa
 
-// console.log(wordArray);
+    // check finale
+    if(word == checkWord){
+        return "palindrome!";
+    } 
+    return "not palindrome!";
+}
 
-// 3. uso array.reverse() per invertire l'ordine dei caratteri dell'array creata
-const reverseWord = wordArray.reverse();
-
-//console.log(reverseWord);
-
-// 4. ricoverto l'array in una stringa per effettuare il controllo - uso array.join per non avere le virgole
-
-const checkWord = reverseWord.join("");
-
-// console.log(checkWord);
-// 5: faccio il check. parola1 = parola1.reverse ? (if/else)
-
-if(word == checkWord){
-    console.log("palindromo!");
-} else (
-    console.log("non palindromo")
-)
+console.log(palindromeCheck(userWord));
